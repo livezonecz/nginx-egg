5850b11ed7ff08d81c6997dd701ec416917ac829
JohnoCZ <55092714+Johno95CZ@users.noreply.github.com>
2023-02-13 17:07:28 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/ash
 rm -rf /home/container/tmp/*
 
-echo "⟳ Starting PHP-FPM..."
+echo "⟳ ZApínání potřebných systémů"
 /usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
-echo "⟳ Starting Nginx..."
-echo "✓ Successfully started"
+echo "⟳ Spouštění web hostingu"
+echo "✓ Úspěšné spuštění web hostingu a systémů"
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
