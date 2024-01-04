4eea974f5174459a34cf1ab033f4f4becf20a8d3
Johno <55092714+JohnoCZ@users.noreply.github.com>
2024-01-04 18:51:29 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/ash
-rm -rf /home/container/tmp/*
-
 echo "⟳ Zapínání potřebných systémů"
+rm -rf /home/container/tmp/*
+> /home/container/access.log
+> /home/container/error.log
+echo "⟳ Spouštění web hostingu"
 /usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
-echo "⟳ Spouštění web hostingu"
 echo "✓ Úspěšné spuštění web hostingu a systémů"
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
